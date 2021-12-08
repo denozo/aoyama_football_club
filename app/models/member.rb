0fa81e72daaeffc3c1e61cbbcd8612d99540044a
denozo <n.demachi23@gmail.com>
2021-12-08 01:15:03 +0000

    membersのgarde日本語化

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,6 @@ class Member < ApplicationRecord
     belongs_to :category
     
     attachment :image
-    enum grade: { grade_1st: 0, grade_2nd: 1, grade_3rd: 2, stuff: 3}
+    enum grade: { grade_1st: 0, grade_2nd: 1, grade_3rd: 2, staff: 3}
 
 end
