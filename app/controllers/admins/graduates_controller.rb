44ad5bb4dfc117c7573438673947dc5e066fcaf2
denozo <n.demachi23@gmail.com>
2021-12-09 10:39:48 +0000

    OB紹介投稿機能実装

--- app/controllers/admins/graduates_controller.rb
+++ app/controllers/admins/graduates_controller.rb
@@ -3,12 +3,17 @@ class Admins::GraduatesController < ApplicationController
   end
 
   def new
+    @graduate = Graduate.new
   end
 
   def create
+    graduate = Graduate.new(graduate_params)
+    graduate.save
+    redirect_to admins_graduate_path(graduate)
   end
 
   def show
+    @graduate = Graduate.find(params[:id])
   end
 
   def edit
@@ -19,4 +24,11 @@ class Admins::GraduatesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def graduate_params
+    params.require(:graduate).permit(:image, :year, :member_list, :career_after_graduation)
+  end
+
 end
