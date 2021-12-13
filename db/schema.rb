7dc6c6a69c6af8cca912f3fd3bc292b30464bed0
denozo <n.demachi23@gmail.com>
2021-12-13 11:43:39 +0000

    itemモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_100507) do
+ActiveRecord::Schema.define(version: 2021_12_13_113802) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -85,6 +85,16 @@ ActiveRecord::Schema.define(version: 2021_12_12_100507) do
     t.string "title", null: false
     t.text "content", null: false
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image_id", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
