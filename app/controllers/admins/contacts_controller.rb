0e9c96882ca72eb5f7d1eca617095151a39ed5ac
denozo <n.demachi23@gmail.com>
2022-01-10 01:58:37 +0000

    お問い合わせ管理レイアウト調整完了

--- app/controllers/admins/contacts_controller.rb
+++ app/controllers/admins/contacts_controller.rb
@@ -3,7 +3,7 @@ class Admins::ContactsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @contacts = Contact.all
+    @contacts = Contact.page(params[:page]).per(10)
   end
 
   def show
